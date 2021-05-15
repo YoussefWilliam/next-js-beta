af335aca15b94ae1fd618f55bf516830cbf40164
Youssef William <youssef.william997@gmail.com>
2021-05-15 15:53:08 +0200

    Rendering different components

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
-import Head from "next/head";
-import Image from "next/image";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>My homepage</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, quod molestiae repellendus eaque fuga quae itaque iure dicta debitis eius, cum vero esse eveniet porro tempore, cumque nobis culpa iste?</p>
+    <div>
+      <Navbar />
+      <div >
+        <h1>My homepage</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, quod
+          molestiae repellendus eaque fuga quae itaque iure dicta debitis eius,
+          cum vero esse eveniet porro tempore, cumque nobis culpa iste?
+        </p>
+      </div>
+      <Footer />
     </div>
   );
 }
